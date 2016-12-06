bf6d03d294f3eda0f582c387007634d45b046ac8
pimpmypixel <pimpmypixel@me.com>
2016-12-06 14:43:58 +0000

    mew index 2

--- dejavu/recognize.py
+++ dejavu/recognize.py
@@ -13,98 +13,98 @@ import dejavu.decoder as decoder
 
 
 class BaseRecognizer(object):
-    def __init__(self, dejavu):
-        self.dejavu = dejavu
+	def __init__(self, dejavu):
+		self.dejavu = dejavu
 
-    def _recognize(self, *data):
-        matches = []
-        log.info('_recognize')
-        for d in data:
-            matches.extend(
-                self.dejavu.find_matches(d))
-        return self.dejavu.align_matches(matches)
+	def _recognize(self, *data):
+		matches = []
+		log.info('_recognize')
+		for d in data:
+			matches.extend(
+				self.dejavu.find_matches(d))
+		return self.dejavu.align_matches(matches)
 
-    def recognize(self):
-        pass  # base class does nothing
+	def recognize(self):
+		pass  # base class does nothing
 
 
 class MicrophoneRecognizer(BaseRecognizer):
-    # hardware dependent
-    default_chunksize = 8096
-    default_format = pyaudio.paInt16
-    default_channels = 1
-    default_samplerate = 44100
+	# hardware dependent
+	default_chunksize = 8096
+	default_format = pyaudio.paInt16
+	default_channels = 1
+	default_samplerate = 44100
 
-    def __init__(self, dejavu):
-        log.info('__init__ MicrophoneRecognizer')
-        super(MicrophoneRecognizer, self).__init__(dejavu)
-        self.starttime = datetime.now()
-        self.audio = pyaudio.PyAudio()
-        self.stream = None
-        self.data = []
-        self.channels = MicrophoneRecognizer.default_channels
-        self.chunksize = MicrophoneRecognizer.default_chunksize
-        self.samplerate = MicrophoneRecognizer.default_samplerate
-        self.recorded = False
+	def __init__(self, dejavu):
+		log.info('__init__ MicrophoneRecognizer')
+		super(MicrophoneRecognizer, self).__init__(dejavu)
+		self.starttime = datetime.now()
+		self.audio = pyaudio.PyAudio()
+		self.stream = None
+		self.data = []
+		self.channels = MicrophoneRecognizer.default_channels
+		self.chunksize = MicrophoneRecognizer.default_chunksize
+		self.samplerate = MicrophoneRecognizer.default_samplerate
+		self.recorded = False
 
-    #    @profile
-    def start_recording(self, channels=default_channels,
-                        samplerate=default_samplerate,
-                        chunksize=default_chunksize):
-        self.chunksize = chunksize
-        self.channels = channels
-        self.recorded = False
-        self.samplerate = samplerate
+	#    @profile
+	def start_recording(self, channels=default_channels,
+						samplerate=default_samplerate,
+						chunksize=default_chunksize):
+		self.chunksize = chunksize
+		self.channels = channels
+		self.recorded = False
+		self.samplerate = samplerate
 
-        if self.stream:
-            self.stream.stop_stream()
-            self.stream.close()
+		if self.stream:
+			self.stream.stop_stream()
+			self.stream.close()
 
-        log.debug('start_recording')
-        self.stream = self.audio.open(
-            format=self.default_format,
-            channels=self.channels,
-            rate=self.samplerate,
-            input=True,
-            frames_per_buffer=self.chunksize,
-            input_device_index=1
-        )
+		log.debug('start_recording')
+		self.stream = self.audio.open(
+			format=self.default_format,
+			channels=self.channels,
+			rate=self.samplerate,
+			input=True,
+			frames_per_buffer=self.chunksize,
+			input_device_index=1
+		)
 
-        self.data = [[] for i in range(channels)]
+		self.data = [[] for i in range(channels)]
 
-    def process_recording(self):
-        log.debug('process_recording')
-        data = self.stream.read(self.chunksize)
-        nums = np.fromstring(data, np.int16)
-        for c in range(self.channels):
-            self.data[c].extend(nums[c::self.channels])
+	def process_recording(self):
+		log.debug('process_recording')
+		data = self.stream.read(self.chunksize)
+		nums = np.fromstring(data, np.int16)
+		for c in range(self.channels):
+			self.data[c].extend(nums[c::self.channels])
 
-    def stop_recording(self):
-        log.debug('stop_recording')
-        self.stream.stop_stream()
-        self.stream.close()
-        self.stream = None
-        self.recorded = True
+	def stop_recording(self):
+		log.debug('stop_recording')
+		self.stream.stop_stream()
+		self.stream.close()
+		self.stream = None
+		self.recorded = True
 
-    def recognize_recording(self):
-        log.info('recognize_recording')
-        if not self.recorded:
-            raise NoRecordingError("Recording was not complete/begun")
-        return self._recognize(*self.data)
+	def recognize_recording(self):
+		log.info('recognize_recording')
+		if not self.recorded:
+			raise NoRecordingError("Recording was not complete/begun")
+		return self._recognize(*self.data)
 
-    def get_recorded_time(self):
-        log.info('get_recorded_time')
-        return len(self.data[0]) / self.rate
+	def get_recorded_time(self):
+		log.info('get_recorded_time')
+		return len(self.data[0]) / self.rate
 
-    def recognize(self, seconds=2):
-        log.debug('recognize')
-        self.start_recording()
-        for i in range(0, int(self.samplerate / self.chunksize
-                                      * seconds)):
-            self.process_recording()
-        self.stop_recording()
-        return self.recognize_recording()
+	def recognize(self, seconds=2):
+		log.debug('recognize')
+		self.start_recording()
+		for i in range(0, int(self.samplerate / self.chunksize
+									  * seconds)):
+			self.process_recording()
+		self.stop_recording()
+		return self.recognize_recording()
 
 
 class NoRecordingError(Exception):
-    pass
+	pass
--- loop1_mysql.py
+++ loop1_mysql.py
@@ -7,6 +7,7 @@ import logging
 import time
 import MySQLdb
 import MySQLdb.cursors
+from ctypes import *
 from datetime import datetime
 from dejavu import Dejavu
 from dejavu.recognize import MicrophoneRecognizer
@@ -16,86 +17,76 @@ warnings.filterwarnings("ignore")
 db = "conf/database.json"
 config = {}
 
+
+def getConguration():
+	with open(os.path.dirname(__file__) + db) as f:
+		logging.debug("Getting config")
+		config['database'] = json.load(f)
+		con = MySQLdb.connect(
+			config.get('database').get('host'),
+			config.get('database').get('user'),
+			config.get('database').get('passwd'),
+			config.get('database').get('db'),
+			cursorclass=MySQLdb.cursors.DictCursor
+		)
+		cur = con.cursor()
+		cur.execute("SELECT * FROM `configurations` WHERE id = (SELECT active FROM `states` ORDER BY id DESC limit 1)")
+		config['fingerprint'] = cur.fetchone()
+
+		config['fingerprint']['amp_min'] = 10
+		config['fingerprint']['plot'] = 0
+		config['verbose'] = False
+
+		config['soundcard'] = {
+			"chunksize": 8096,
+			"test": 1
+		}
+		return config
+
+
 try:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', action='count', default=0)
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-v', '--verbose', action='count', default=0)
+	args = parser.parse_args()
 
-    levels = [logging.NOTSET, logging.INFO, logging.DEBUG]
-    level_index = min(len(levels) - 1, args.verbose)
-    level = levels[level_index]  # capped to number of levels
-    logging.basicConfig(level=level, format="%(asctime)s %(levelname)s %(message)s")
+	levels = [logging.WARNING, logging.INFO, logging.DEBUG]
+	level_index = min(len(levels) - 1, args.verbose)
+	level = levels[level_index]  # capped to number of levels
+	logging.basicConfig(filename='log.log',
+						filemode='a',
+						format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+						datefmt='%H:%M:%S',
+						level=level)
 
-    with open(os.path.dirname(__file__) + db) as f:
-        config['database'] = json.load(f)
-        con = MySQLdb.connect(
-            config.get('database').get('host'),
-            config.get('database').get('user'),
-            config.get('database').get('passwd'),
-            config.get('database').get('db'),
-            cursorclass=MySQLdb.cursors.DictCursor
-        )
-        cur = con.cursor()
-        cur.execute("SELECT * FROM `configurations` WHERE id = (SELECT active FROM `states` ORDER BY id DESC limit 1)")
-        config['fingerprint'] = cur.fetchone()
+	config = getConguration()
 
-        config['fingerprint']['amp_min'] = 10
-        config['fingerprint']['plot'] = 0
-        config['verbose'] = False
+	logging.info("Using config '" + config['fingerprint']['name'] + "'")
+	
 
-        config['soundcard'] = {
-            "chunksize": 8096,
-            "test": 1
-        }
-
-        logging.info("Using config '" + config['fingerprint']['name'] + "'")
-        if args.verbose:
-            config['verbose'] = True
-
-        if config['verbose']:
-            import pyaudio
-
-            p = pyaudio.PyAudio()
-            for i in range(0, 3):
-                if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                    logging.debug("Input Device id " + str(i) + " - " +
-                                  p.get_device_info_by_host_api_device_index(0, i).get('name'))
-
-            devinfo = p.get_device_info_by_index(1)
-            logging.info("Selected device is " + str(devinfo.get('name')))
-            if p.is_format_supported(44100.0,  # Sample rate
-                                     input_device=devinfo["index"],
-                                     input_channels=devinfo['maxInputChannels'],
-                                     input_format=pyaudio.paInt16):
-                logging.debug('Hardware config supported')
-            else:
-                logging.info('Hardware config not supported')
-                sys.exit(1)
-
-        if __name__ == '__main__':
-            djv = Dejavu(config)
-            a = datetime.now()
-            listen = 2
-            pause = 1
-            it = 1
-            try:
-                while True:
-                    song = djv.recognize(MicrophoneRecognizer, seconds=listen)
-                    if song is None:
-                        if args.verbose:
-                            logging.info(str(it) + ". Nothing recognized")
-                        else:
-                            print str(it) + " - Nothing recognized"
-                    else:
-                        if args.verbose:
-                            logging.info(str(it) + ". Recognized from mic with %d seconds: %s\n" % (listen, song))
-                        else:
-                            print str(it) + " - Recognized from mic with %d seconds: %s\n" % (listen, song)
-                    it += 1
-                    time.sleep(pause)
-            except KeyboardInterrupt:
-                pass
+	if __name__ == '__main__':
+		djv = Dejavu(config)
+		a = datetime.now()
+		listen = 2
+		pause = 1
+		it = 1
+		try:
+			while True:
+				song = djv.recognize(MicrophoneRecognizer, seconds=listen)
+				if song is None:
+					if args.verbose:
+						logging.info(str(it) + ". Nothing recognized")
+					else:
+						print str(it) + " - Nothing recognized"
+				else:
+					if args.verbose:
+						logging.info(str(it) + ". Recognized from mic with %d seconds: %s\n" % (listen, song))
+					else:
+						print str(it) + " - Recognized from mic with %d seconds: %s\n" % (listen, song)
+				it += 1
+				time.sleep(pause)
+		except KeyboardInterrupt:
+			pass
 
 except MySQLdb.Error, e:
-    print "Error %d: %s" % (e.args[0], e.args[1])
-    sys.exit(1)
+	logging.info("Error %d: %s" % (e.args[0], e.args[1]))
+	sys.exit(1)
